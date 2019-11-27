b07838d845e93b5c45b0a5b39d926de05d0ba685
carlosnight02 <ca.murillo1@hotmail.com>
2019-11-27 17:03:01 -0500

    tests postman

--- controllers/CategoriaController.js
+++ controllers/CategoriaController.js
@@ -2,26 +2,26 @@ import models from '../models';
 export default{
     add: async(req, res,next) =>{
         try{
-            const reg= await models.Categoria.create(req.boby);
+            const reg= await models.Categoria.create(req.body);
             res.status(200).json(reg);
         }catch(e){
-            res.status(500).send({
+                res.status(500).send({
                 messge: 'Ocurrio un error'
             });
             next(e);
         }
     },
-    query: async(req, res,next) =>{
+    query: async(req,res,next) =>{
         try{
-            const reg= await models.Categoria.findOne({_id:req.query._id})
+            const reg= await models.Categoria.findOne({_id:req.query._id});
+           
             if(!reg){
-                res.status(404).send({
-                    messge: 'El registro no existe'
-                });
+                res.status(404).send({ message: 'El registro no existe' });
             }else{
                 res.status(200).json(reg);
             }
         }catch(e){
+            
             res.status(500).send({
                 messge: 'Ocurrio un error'
             });
@@ -30,7 +30,8 @@ export default{
     },
     list: async(req, res,next) =>{
         try{
-            const reg= await models.Categoria.find({});
+            let valor=req.query.valor;
+            const reg= await models.Categoria.find({$or:[{'nombre':new RegExp(valor, 'i')},{'descripcion': new RegExp(valor, 'i') }]},{}).sort({'createAt': -1});
             res.status(200).json(reg);
         }catch(e){
             res.status(500).send({
@@ -42,7 +43,7 @@ export default{
     },
     update: async(req, res,next) =>{
         try{
-            const reg = await models.Categoria.findByIdAndUpdate({_id:req.boby._id}, {nombre:req.body.nombre, descripcion:req.body.descripcion})
+            const reg = await models.Categoria.findByIdAndUpdate({_id:req.body._id}, {nombre:req.body.nombre, descripcion:req.body.descripcion});
             res.status(200).json(reg);
 
         }catch(e){
